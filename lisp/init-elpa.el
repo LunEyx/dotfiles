05aa60b75fd3339f25ea2ea8842266d2604b4962
LunEyx <luneyxlab@gmail.com>
2018-02-01 17:41:33 +0800

    Fix bug again

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -10,17 +10,32 @@
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (when (< emacs-major-version 24)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://melpa-stable.milkbox.net/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 ;; ----------------------------------------------
 ;; Function
 ;; ----------------------------------------------
 
 ;; Install package if not installed
-(defun require-package (package)
-  (unless (package-installed-p package)
-    (package-install package)))
+;;(defun require-package (package)
+;;  (unless (package-installed-p package)
+;;    (package-install package nil)))
+
+(defun require-package (package &optional min-version no-refresh)
+    "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+	(if (package-installed-p package min-version)
+		t
+	  (if (or (assoc package package-archive-contents) no-refresh)
+		  (if (boundp 'package-selected-packages)
+			  ;; Record this as a package the user installed explicitly
+			  (package-install package nil)
+			(package-install package))
+		(progn
+		  (package-refresh-contents)
+		          (require-package package min-version t)))))
 
 ;; Install packages if not installed
 (defun require-packages (&rest packages)
